464697d346924001a3b28d9bf73fbad4520ca0ac
Eli <eli.serra173@gmail.com>
2026-05-06 20:18:19 +0200

    Add KeyResolver to public API in security module

--- auth_sdk_m8/security/__init__.py
+++ auth_sdk_m8/security/__init__.py
@@ -1,11 +1,13 @@
 """Security validation interfaces for auth-sdk-m8."""
 
+from auth_sdk_m8.security.key_resolver import KeyResolver
 from auth_sdk_m8.security.session_store import SessionStore
 from auth_sdk_m8.security.token_policy import TokenPolicy
 from auth_sdk_m8.security.token_validator import TokenValidator
 from auth_sdk_m8.security.validation import TokenValidationConfig
 
 __all__ = [
+    "KeyResolver",
     "SessionStore",
     "TokenPolicy",
     "TokenValidator",
