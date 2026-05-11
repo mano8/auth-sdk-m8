0d8bc5944421461f3bd26f48a9d8992e5ba8f48f
Eli <eli.serra173@gmail.com>
2026-05-11 10:50:20 +0200

    feat(exceptions): add ConfigurationError for fatal application configuration issues

--- auth_sdk_m8/core/exceptions.py
+++ auth_sdk_m8/core/exceptions.py
@@ -3,3 +3,6 @@
 
 class InvalidToken(Exception):
     """Raised when a JWT token is invalid, expired, or of the wrong type."""
+
+class ConfigurationError(RuntimeError):
+    """Fatal application configuration error."""
